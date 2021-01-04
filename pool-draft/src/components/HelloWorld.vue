b51cc3cfd7fa981bff2722aa874ee5536ffd4a32
Sebastien <morneausebastien@gmail.com>
2021-01-03 21:19:05 -0500

    - Add 2013 - Correct some words

--- pool-draft/src/components/HelloWorld.vue
+++ pool-draft/src/components/HelloWorld.vue
@@ -2,21 +2,28 @@
   <v-container>
     <v-card elevation="9" shaped>
       <v-divider class="mx-4"></v-divider>
-      <v-card-title>11e édition - 2020</v-card-title>
+      <v-card-title>11ᵉ édition - 2020</v-card-title>
       <v-divider class="mx-4"></v-divider>
-      <v-card-title>10e édition - 2019</v-card-title>
+      <v-card-title>10ᵉ édition - 2019</v-card-title>
       <v-divider class="mx-4"></v-divider>
-      <v-card-title>9e édition - 2018</v-card-title>
+      <v-card-title>9ᵉ édition - 2018</v-card-title>
       <v-divider class="mx-4"></v-divider>
-      <v-card-title>8e édition - 2017</v-card-title>
+      <v-card-title>8ᵉ édition - 2017</v-card-title>
       <v-divider class="mx-4"></v-divider>
-      <v-card-title>7e édition - 2016</v-card-title>
+      <v-card-title>7ᵉ édition - 2016</v-card-title>
       <v-divider class="mx-4"></v-divider>
-      <v-card-title>6e édition - 2015</v-card-title>
+      <v-card-title>6ᵉ édition - 2015</v-card-title>
       <v-divider class="mx-4"></v-divider>
-      <v-card-title>5e édition - 2014</v-card-title>
+      <v-card-title>5ᵉ édition - 2014</v-card-title>
+      <v-data-table
+        class="elevation-1"
+        :headers="headers_G"
+        :items="data2014"
+        :items-per-page="itemsPerPage"
+        hide-default-footer
+      ></v-data-table>
       <v-divider class="mx-4"></v-divider>
-      <v-card-title>4e édition - 2013</v-card-title>
+      <v-card-title>4ᵉ édition - 2013</v-card-title>
       <v-data-table
         class="elevation-1"
         :headers="headers"
@@ -25,7 +32,7 @@
         hide-default-footer
       ></v-data-table>
       <v-divider class="mx-4"></v-divider>
-      <v-card-title>3e édition - 2012</v-card-title>
+      <v-card-title>3ᵉ édition - 2012</v-card-title>
       <v-data-table
         class="elevation-1"
         :headers="headers"
@@ -34,7 +41,7 @@
         hide-default-footer
       ></v-data-table>
       <v-divider class="mx-4"></v-divider>
-      <v-card-title>2e édition - 2011</v-card-title>
+      <v-card-title>2ᵉ édition - 2011</v-card-title>
       <v-data-table
         class="elevation-1"
         :headers="headers"
@@ -43,7 +50,7 @@
         hide-default-footer
       ></v-data-table>
       <v-divider class="mx-4"></v-divider>
-      <v-card-title>1ère édition - 2010</v-card-title>
+      <v-card-title>1ᵉʳᵉ édition - 2010</v-card-title>
       <v-data-table
         class="elevation-1"
         :headers="headers"
@@ -60,6 +67,7 @@ import pool2010 from "../data/pool_2010.json";
 import pool2011 from "../data/pool_2011.json";
 import pool2012 from "../data/pool_2012.json";
 import pool2013 from "../data/pool_2013.json";
+import pool2014 from "../data/pool_2014.json";
 
 export default {
   name: "HelloWorld",
@@ -83,10 +91,31 @@ export default {
       { text: "ECM", value: "ECM" },
       { text: "EC", value: "EC" },
     ],
+    headers_G: [
+      {
+        text: "Rang",
+        align: "start",
+        value: "Rank",
+      },
+      { text: "Nom", value: "Name" },
+      { text: "PJ", value: "PJ" },
+      { text: "PTS", value: "PTS" },
+      { text: "BTS", value: "BTS" },
+      { text: "PAS", value: "PAS" },
+      { text: "VIC", value: "VIC" },
+      { text: "DP", value: "DP" },
+      { text: "BL", value: "BL" },
+      { text: "Ratio", value: "Ratio" },
+      { text: "PUN", value: "PUN" },
+      { text: "SAL", value: "SAL" },
+      { text: "ECM", value: "ECM" },
+      { text: "EC", value: "EC" },
+    ],
     data2010: pool2010,
     data2011: pool2011,
     data2012: pool2012,
     data2013: pool2013,
+    data2014: pool2014,
   }),
 };
 </script>
