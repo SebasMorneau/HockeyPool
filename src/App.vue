ff38ed7c60ca3bdcdc516168ed994ebd4f1a1cdc
Sebastien <morneausebastien@gmail.com>
2021-01-06 11:35:39 -0500

    test2

--- src/App.vue
+++ src/App.vue
@@ -3,7 +3,7 @@
     <v-app-bar app color="primary" dark>
       <v-container>
         <v-row justify="center" align="center" class="navbar_main">
-          POOL JOLIETTE ET CIE Emma stone
+          POOL JOLIETTE ET CIE
         </v-row>
       </v-container>
     </v-app-bar>
