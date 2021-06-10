b3a0c11467bca770b0a6a72088f87919a05224ae
Sebastien <morneausebastien@gmail.com>
2021-06-09 23:17:32 -0400

    Changes the links for Facebook and pool.com

--- src/components/Footer.vue
+++ src/components/Footer.vue
@@ -2,14 +2,22 @@
   <v-footer color="primary lighten-1" padless>
     <v-row justify="center" no-gutters>
       <v-btn
-        v-for="link in links"
-        :key="link"
         color="white"
         text
         rounded
         class="my-2"
+        href="https://www.facebook.com/groups/412050072205778/"
       >
-        {{ link }}
+        Facebook
+      </v-btn>
+      <v-btn
+        color="white"
+        text
+        rounded
+        class="my-2"
+        href="https://www.lepool.com/pool/52469"
+      >
+        Pool{{ new Date().getFullYear() }}.com
       </v-btn>
       <v-col class="primary py-4 text-center white--text" cols="12">
         {{ new Date().getFullYear() }} — <strong>Pool Joliette et cie</strong>
